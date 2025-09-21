d1d2439cc0cf7e9dd4693403ff20d32c6c10b444
hduc-le <hduc.lee@gmail.com>
2025-09-21 19:31:47 +0700

    update gazelle build from import

---
+++ app/python/src/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def generate_random_int_data():
+    return np.random.randint(100, 1000, 4)
+
+
+def generate_random_uniform_data():
+    return np.random.uniform(0.1, 0.5, 4)
